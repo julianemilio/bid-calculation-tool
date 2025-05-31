e7fe58b7b62e0b5ca35c4cccca038b9abfd4fe40
CO029397 <CO029397@gmodelo.com.mx>
2025-05-31 18:19:01 -0500

    feat(domain): implement SpecialFeeCalculator to calculate special fees

--- BidCalculationTool/Domain/FeeCalculators/BasicFeeCalculator.cs
+++ BidCalculationTool/Domain/FeeCalculators/BasicFeeCalculator.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Enums;
+using Domain.Exeptions;
 
 namespace Domain.FeeCalculators
 {
     internal class BasicFeeCalculator
     {
+        public decimal Calculate(decimal price, VehicleType vehicleType)
+        {
+            if (price <= 0) throw new InvalidPriceException();
+
+            decimal percentage = 0.10m;
+            decimal fee = price * percentage;
+
+            return vehicleType switch
+            {
+                VehicleType.Common => Math.Clamp(fee, 10, 50),
+                VehicleType.Luxury => Math.Clamp(fee, 25, 200),
+                _ => throw new ArgumentException("Invalid vehicle type")
+            };
+        }
     }
 }
--- BidCalculationTool/Domain/FeeCalculators/SpecialFeeCalculator.cs
+++ BidCalculationTool/Domain/FeeCalculators/SpecialFeeCalculator.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Enums;
+using Domain.Exeptions;
 
 namespace Domain.FeeCalculators
 {
     internal class SpecialFeeCalculator
     {
+        public decimal Calculate(decimal price, VehicleType vehicleType)
+        {
+            if (price <= 0) throw new InvalidPriceException();
+
+            return vehicleType switch
+            {
+                VehicleType.Common => price * 0.02m, // 2%
+                VehicleType.Luxury => price * 0.04m, // 4%
+                _ => throw new ArgumentException("Invalid vehicle type")
+            };
+        }
     }
 }
